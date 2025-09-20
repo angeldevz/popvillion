74a30b2d3c642f971d4de6cbe40589147157a3c8
Rustan Corpuz <ruco@splitmedialabs.com>
2025-09-21 01:14:49 +0800

    feat: GraphQL API server

--- packages/api/dist/schema.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.schema = void 0;
-const builder_1 = require("./builder");
-require("./query");
-// Export a **GraphQLSchema**
-exports.schema = builder_1.builder.toSchema();
